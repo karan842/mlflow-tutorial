3052c2c760ca59ad85e3e70e79fa5f7e9d47ea2b
karan842 <karanshingde@gmail.com>
2025-08-07 13:53:30 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+import mlflow 
+
+print("printing tracking URI scheme below")
+print(mlflow.get_tracking_uri())
+print("/n")
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+print("Printing now tracking URI scheme below")
+print(mlflow.get_tracking_uri())
+print("\n")
